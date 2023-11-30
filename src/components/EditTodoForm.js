92024daaeac95c817c4e75e66e99918a266ae348
deveshtiwari <devesht709@gmail.com>
2023-11-30 11:45:35 +0530

    commmited

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,18 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const EditTodoForm = ({editTodo, task}) => {
-    const [value, setValue] = useState(task.task);
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        // edit todo
-        editTodo(value, task.id);
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (typeof editTodo === 'function' && task) {
+      // Edit todo
+      editTodo(value, task.id);
+      // Reset form state
+      setValue('');
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
-    <button type="submit" className='todo-btn'>Add Task</button>
-  </form>
-  )
-}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="Update task"
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
+
+EditTodoForm.propTypes = {
+  editTodo: PropTypes.func.isRequired,
+  task: PropTypes.object.isRequired,
+};
